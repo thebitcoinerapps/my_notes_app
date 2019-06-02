5481533d5043f92c633e87fbdfe11ad1541347fd
theBitCoiner <thebitcoinerapps@gmail.com>
2019-06-02 11:42:44 +0200

    removing and printig notes functinality added

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const yargs = require('yargs');
 const chalk = require('chalk');
 
-const addNote = require('./utils');
+const utils = require('./utils');
+console.log(chalk.yellow.inverse('Weclome to notes app! insert node app.js --help for help'));
+console.log('\n');
 
 yargs.version('1.0.0');
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
     handler: (argv)=>{
-        addNote(argv.title, argv.body);
+        utils.addNote(argv.title, argv.body);
     }
 });
-console.log(yargs.argv);
+
+yargs.command({
+    command: 'print',
+    describe: 'Show all notes',
+    handler: ()=>{
+        utils.printNotes();
+    }
+});
+yargs.command({
+    command: 'remove',
+    describe: 'remove selected note',
+    handler: (argv)=>{
+        utils.removeNotes(argv.num);
+    }
+});
+
+console.log(chalk.black(yargs.argv));
--- utils.js
+++ utils.js
@@ -1,31 +1,72 @@
 const fs = require('fs');
+const chalk = require('chalk');
 
 
 const notesArray = [];
 
 const addNote = (title, body)=>{
-    try{
-        const note = {
+    
+    if(!fs.existsSync('notes.json')){
+        fs.writeFileSync('notes.json','');
+    
+    notesArray.push({
+        title,
+        body
+    });
+    const arrayJSON = JSON.stringify(notesArray);
+    fs.writeFileSync('notes.json', arrayJSON);
+    }else{
+        const savedNotes = fs.readFileSync('notes.json');
+        const saveNotesStr = savedNotes.toString();
+        const saveNotesObj = JSON.parse(saveNotesStr);
+        
+        saveNotesObj.push({
             title,
             body
-        }
-        const noteInJSON = JSON.stringify(note);
-        notesArray.push(noteInJSON);
-        console.log(notesArray);
-    
-        fs.writeFileSync('notes.json', notesArray);
-    }catch{
-        const note = {
-            title,
-            body
-        }
-        const noteInJSON = JSON.stringify(note);
-        notesArray.push(noteInJSON);
-        console.log(notesArray);
-    
-        fs.writeFileSync('notes.json', notesArray);
+        });
+
+        const updatedArr = JSON.stringify(saveNotesObj);
+        fs.writeFileSync('notes.json', updatedArr);
+       
+    }
+}
+
+const printNotes =()=>{
+    if(fs.existsSync('notes.json')){
+        const notes = fs.readFileSync('notes.json');
+        const notesString = notes.toString();
+        const notesObj = JSON.parse(notesString);
+        console.log(notesObj);
+    }else{
+        console.log('There are no notes to print');
+    }
+}
+
+const removeNotes =(num)=>{
+    if(fs.existsSync('notes.json')){
+        const notes = fs.readFileSync('notes.json');
+        const notesString = notes.toString();
+        const notesObj = JSON.parse(notesString);
+            for(let i = 0; i<notesObj.length; i++){
+                console.log(`${i} t: ${notesObj[i].title} b:${notesObj[i].body}`);
+                console.log('----------------------------');
+            }
+            console.log(`Please use ${chalk.yellow.inverse('node app.js remove --num=number of note to be removed')}`);
+                if(num){
+                    const newArr = notesObj.splice(num,1);
+                    const newArrToBeSaved = JSON.stringify(newArr);
+                    fs.writeFileSync('notes.json', newArrToBeSaved);
+                }
+
+    }else{
+        console.log('There is no notes to remove');
     }
 
 }
 
-module.exports = addNote;
+
+module.exports = {
+    addNote,
+    printNotes,
+    removeNotes
+}
